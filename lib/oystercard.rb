6fdd2323e6a7e8df032c3825665815b8cf96c7b7
brock789 <brockway789@googlemail.com>
2021-07-26 16:38:23 +0100

    Step 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,16 @@
+
 class Oystercard
-  attr_reader :balance
+  CARDLIMIT = 90
+  attr_reader :balance, :cardlimit
 
   def initialize
     @balance = 0
+    @cardlimit = CARDLIMIT
   end
 
   def top_up(number)
-    @balance += number
+  fail "Not topped up, over £#{CARDLIMIT} limit" if (@balance += number) > CARDLIMIT 
+  "test" 
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,5 +10,11 @@ describe Oystercard do
       my_card.top_up(5)
       expect(my_card.balance).to eq 5
     end
+    it "top_up does not allow a balance over £90" do
+      my_card = Oystercard.new
+      maximum_balance = Oystercard::CARDLIMIT
+      subject.top_up maximum_balance
+      expect { subject.top_up 1 }.to raise_error("Not topped up, over £#{maximum_balance} limit")
+    end
   end
 end
