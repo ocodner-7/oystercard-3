fa5bdfe4d9b840719159299a5e0c18b8f67872c7
Beckie Doyle <beckiedoyle@hotmail.co.uk>
2021-07-26 16:03:09 +0100

    Up to chapter 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
+  it "has a default value of 0" do
+    expect(Oystercard.new.balance).to eq(0)
+  end
+  describe "#top_up" do
+    it "tops up my balance with the amount stated" do
+      my_card = Oystercard.new
+      my_card.top_up(5)
+      expect(my_card.balance).to eq 5
+    end
+  end
 end
